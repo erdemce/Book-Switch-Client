323b09ea513e9b4140e99e9798a60a1ebb9b09d9
erdemce <erdemce2234@gmail.com>
2021-03-10 17:55:33 +0100

    deployement try 6

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -59,7 +59,7 @@ export default class BookList extends Component {
   render() {
     const { books, filtered } = this.state;
 
-    if(books.length===0){
+    if(books.length===0 || !books || !filtered){
       return <>
       <Spinner animation="border" variant="primary" />
       <Spinner animation="border" variant="secondary" />
